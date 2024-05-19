fcc4214381c7c7a4f4a28571e5b4fac3b633c30c
Jyoumama <juri19771211@gmail.com>
2024-05-19 10:53:59 +0900

    chapter3 3-5まで

---
+++ else_if.rb
@@ -0,0 +1,11 @@
+season = "夏"
+if season == "春"
+  puts "アイスを買って行こう！"
+else
+  if season == "夏"
+    puts "かき氷を買ってこう！"
+  else
+    puts "あんまん買ってこう！"
+  end
+end
+
---
+++ elsif.rb
@@ -0,0 +1,8 @@
+season = "夏"
+if season == "春"
+  puts "アイスを買って行こう！"
+elsif season == "夏"
+  puts "かき氷を買ってこう！"
+else
+  puts "あんまん買ってこう！"
+end
