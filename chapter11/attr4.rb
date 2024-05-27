2429c5d84dddc2013c3edd5118c733384723dc43
Jyoumama <juri19771211@gmail.com>
2024-05-27 11:58:09 +0900

    chapter11-2

---
+++ chapter11/attr4.rb
@@ -0,0 +1,7 @@
+class Drink
+  attr_accessor :name
+end
+
+drink = Drink.new
+drink.name = "日本酒"
+p drink.name
---
+++ chapter11/self2.rb
@@ -0,0 +1,7 @@
+class Drink
+  def self.me
+    p self.object_id
+  end
+
+endp Drink.object_id
+Drink.me
